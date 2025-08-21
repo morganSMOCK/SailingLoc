3d04c0dc0bcd2ac12a70ae1974bb3a7f06464b6b
morganSMOCK <114483278+morganSMOCK@users.noreply.github.com>
2025-08-21 16:34:07 +0200

    Updated BookingService.js

--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -5,7 +5,7 @@
 export class BookingService {
   constructor() {
     // URL de base de l'API - utilise Render en production
-    this.baseURL = 'https://sailingloc-backend.onrender.com/api';
+    this.baseURL = 'https://sailingloc.onrender.com/api';
     this.bookingsEndpoint = `${this.baseURL}/bookings`;
   }
 
