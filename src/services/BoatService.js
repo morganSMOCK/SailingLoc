223ee743fc3868f39ae47acbc3fe99dc5c1972c9
morganSMOCK <114483278+morganSMOCK@users.noreply.github.com>
2025-08-22 09:35:17 +0200

    Fix BoatService API URL for local development

--- src/services/BoatService.js
+++ src/services/BoatService.js
@@ -5,7 +5,7 @@
 export class BoatService {
   constructor() {
     // URL de base de l'API - utilise Render en production
-    this.baseURL = 'https://sailingloc.onrender.com/api';
+    this.baseURL = '/api';
     this.boatsEndpoint = `${this.baseURL}/boats`;
   }
 
