59c636dc8d9e3bf90a96c84fab3a06e4703bce04
morganSMOCK <114483278+morganSMOCK@users.noreply.github.com>
2025-08-22 09:36:18 +0200

    Correction de l'URL API dans BoatService

--- src/services/BoatService.js
+++ src/services/BoatService.js
@@ -5,7 +5,7 @@
 export class BoatService {
   constructor() {
     // URL de base de l'API - utilise Render en production
-    this.baseURL = '/api';
+    this.baseURL = 'https://sailingloc.onrender.com/api';
     this.boatsEndpoint = `${this.baseURL}/boats`;
   }
 
