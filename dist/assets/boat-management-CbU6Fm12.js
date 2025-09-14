aaf6db0436f8d6540d0763b7531aec37f1f25d64
Morgan <m.smock973@gmail.com>
2025-09-14 11:55:07 +0200

    fix

--- dist/assets/boat-management-CbU6Fm12.js
+++
@@ -1,1 +0,0 @@
-import{S as n}from"./main-CJu-3l90.js";/* empty css              */window.app=new n;document.addEventListener("DOMContentLoaded",()=>{window.app&&window.app.initBoatManagement()});
---
+++ dist/assets/boat-management-iKfG47GH.js
@@ -0,0 +1,1 @@
+import{S as n}from"./main-B1ZTtwxc.js";/* empty css              */window.app=new n;document.addEventListener("DOMContentLoaded",()=>{window.app&&window.app.initBoatManagement()});
